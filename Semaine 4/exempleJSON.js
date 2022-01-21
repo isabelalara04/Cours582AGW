8c78851248b894217b49182e9931621aef4eeecc
geocot <martin.couture@uqtr.ca>
2022-01-21 13:29:56 -0500

    Ajout

---
+++ Semaine 4/exempleJSON.js
@@ -0,0 +1,16 @@
+{
+    "plantes":[
+        {
+            "id":1,
+            "nom": "Tournesol",
+            "zone":4,
+            "prix":3.99
+        }, 
+        {
+            "id":2,
+            "nom": "Navet",
+            "zone":5,
+            "prix":1.99
+        }
+    ]
+}
